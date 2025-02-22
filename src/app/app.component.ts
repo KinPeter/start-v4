2d1cecf69079842c2e4042a07a119afb9b97f7cb
Peter Kin <kinpeter85@gmail.com>
2025-02-22 21:52:51 +0100

    Add shared worker service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AuthService } from './auth/auth.service';
 import { NotificationLogComponent } from './common/notification-log.component';
 import { NotificationService } from './services/notification.service';
 import { environment } from '../environments/environment';
+import { SharedWorkerService } from './services/shared-worker.service';
 
 @Component({
   selector: 'pk-root',
@@ -13,14 +14,18 @@ import { environment } from '../environments/environment';
     <pk-notification-log />`,
   styles: ``,
 })
-export class AppComponent {
-  title = 'start-v4';
-
+export class AppComponent implements OnDestroy {
   constructor(
     private authService: AuthService,
-    private notificationService: NotificationService
+    private notificationService: NotificationService,
+    private sharedWorkerService: SharedWorkerService
   ) {
     this.authService.autoLogin();
+    this.sharedWorkerService.initiate();
     this.notificationService.showLog('Welcome to Start version ' + environment.version);
+  }
+
+  ngOnDestroy(): void {
+    this.sharedWorkerService.close();
   }
 }
